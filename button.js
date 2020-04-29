6ac0e15cd3a66a7935064af45a051eaf00a24e9e
lehoang1488 <64474125+lehoang1488@users.noreply.github.com>
2020-04-30 02:16:10 +0700

    Add files via upload

---
+++ button.js
@@ -0,0 +1,7 @@
+#wrap {margin:20px auto;text-align:center;}
+a.btn {display:inline-block;position:relative;font-family:&#39;Open Sans&#39;,sans-serif;text-decoration:none;font-weight:700;background:#30abd5;letter-spacing:.5px;padding:10px 20px;margin:10px;color:#fff;box-shadow:inset 0 0 0 #22313F;font-size:16px;text-transform:uppercase;border-radius:3px;transition:all 0.3s ease-out;}
+a.btn:hover {background:#30abd5;color:#fff;box-shadow:inset 0px -50px 0px #22313F;}
+a.btn:active {color:#05555e;box-shadow:inset 0px -50px 0px #f5f7fa;}
+a.btn.warn {background:#f5f7fa;color:#05555e;box-shadow:inset 0 0 0 #30abd5;}
+a.btn.warn:hover {background:#f5f7fa;color:#fff;box-shadow:inset 0px -50px 0px #30abd5;}
+a.btn.warn:active {color:#fff;box-shadow:inset 0px -50px 0px #30abd5;} 0px -50px 0px #30abd5;}
